16d6b0a09a61136c502b15bf793942d7b06b0a9f
Nikolay <k@uilim.ru>
2025-02-15 16:48:48 +0800

    Feat: Make routes private

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import { Navigate, Route, Routes } from 'react-router'
 import './App.css'
 import { MainLayout } from './layout/MainLayout'
-import { MainPage } from './page/MainPage/MainPage'
-import { HeroPage } from './page/HeroPage/HeroPage'
-import { LocationPage } from './page/LocationsPage/LocationPage'
+import { EpisodeDetailPage } from './page/EpisodeDetailPage/EpisodeDetailPage'
 import { EpisodePage } from './page/EpisodePage/EpisodePage'
 import { HeroDetailPage } from './page/HeroDetailPage/HeroDetailPage'
+import { HeroPage } from './page/HeroPage/HeroPage'
 import { LocationDetailPage } from './page/LocationDetailPage/LocationDetailPage'
-import { EpisodeDetailPage } from './page/EpisodeDetailPage/EpisodeDetailPage'
+import { LocationPage } from './page/LocationsPage/LocationPage'
+import { LoginPage } from './page/LoginPage/LoginPage'
+import { MainPage } from './page/MainPage/MainPage'
+import { useAuth } from './shared/providers/auth-provider/auth-context'
 
 function App() {
-  return (
-    <Routes>
-      <Route element={<MainLayout />}>
+  const { isAuth } = useAuth()
+
+  let routes: JSX.Element = <></>
+
+  if (isAuth) {
+    routes = (
+      <>
         <Route index element={<MainPage />} />
         <Route path='heros' element={<HeroPage />} />
         <Route path='hero/:id' element={<HeroDetailPage />} />
@@ -21,7 +27,20 @@ function App() {
         <Route path='episodes' element={<EpisodePage />} />
         <Route path='episode/:id' element={<EpisodeDetailPage />} />
         <Route path='*' element={<Navigate to={'/'} />} />
-      </Route>
+      </>
+    )
+  } else {
+    routes = (
+      <>
+        <Route index element={<LoginPage />} />
+        <Route path='*' element={<Navigate to={'/'} />} />
+      </>
+    )
+  }
+
+  return (
+    <Routes>
+      <Route element={<MainLayout />}>{routes}</Route>
     </Routes>
   )
 }
