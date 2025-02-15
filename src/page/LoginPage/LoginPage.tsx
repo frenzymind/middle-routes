291a9bd543be05d626d1ea6aaccd1ca62e5a1412
Nikolay <k@uilim.ru>
2025-02-15 16:48:26 +0800

    Feat: Add login page

---
+++ src/page/LoginPage/LoginPage.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import { useAuth } from '../../shared/providers/auth-provider/auth-context'
+import { Card } from '../../shared/ui/Card/Card'
+import { ILoginFormEvent } from './model/types/login-form'
+
+export function LoginPage() {
+  const auth = useAuth()
+  const [loginError, setLoginError] = useState('')
+
+  const submitLoginForm = (event: React.FormEvent<ILoginFormEvent>) => {
+    event.preventDefault()
+
+    const formElements = event.currentTarget.elements
+
+    const successs = auth.login(formElements.username.value, formElements.pswd.value)
+
+    if (!successs) {
+      setLoginError('Неверные данные. Попробуйте ещё раз.')
+    }
+  }
+
+  return (
+    <Card hover={false}>
+      <form onSubmit={submitLoginForm} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <div aria-hidden='true'>Вход (admin / qwerty)</div>
+        <div style={{ marginTop: '10px' }}>
+          <input type='text' name='username' placeholder='usename' />
+        </div>
+        <div style={{ marginTop: '10px' }}>
+          <input type='password' name='pswd' placeholder='password' />
+        </div>
+        <button style={{ marginTop: '10px' }}>Войти</button>
+        {loginError}
+      </form>
+    </Card>
+  )
+}
