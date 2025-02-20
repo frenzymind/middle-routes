f0575d15561d98b6955814d7a5a9d2b2dbed23e2
Nikolay <k@uilim.ru>
2025-02-21 00:06:49 +0800

    Feat: Add error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router'
 import { AuthProvider } from './shared/providers/auth-provider/auth-provider.tsx'
+import { ErrorBoundary } from './shared/hoc/ErrorBoundary.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <ErrorBoundary>
+          <App />
+        </ErrorBoundary>
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>
